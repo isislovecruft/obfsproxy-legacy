04805f75263fad4a841276b8a4114db3d99f36f8
Nick Mathewson <nickm@torproject.org>
2012-02-10 20:51:02 -0500

    Patch from "wwaites" to build on freebsd

--- src/util.h
+++ src/util.h
@@ -25,6 +25,18 @@
                          but it doesn't) */
 #endif
 
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
 struct bufferevent;
 struct evbuffer;
 struct evconnlistener;
